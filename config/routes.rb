15226decba6e414531d3191c407c264f691a98a5
y-naoto <y.naoto.1441@gmail.com>
2021-09-26 21:45:34 +0900

    itemsコントローラー作成の作成及びホーム画面の仮作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'items/index'
   devise_for :users
+  
+  root to: "items#index"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
+  resources :users, only: [:edit, :update]
 
 end
