4578d392617400c8ad8ff78cef5b098c672af1f5
y-naoto <y.naoto.1441@gmail.com>
2021-10-06 18:31:09 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update]
   def index
     @item =Item.includes(:user)
   end
@@ -7,6 +8,11 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def edit
+    @item =Item.find(params[:id])
+  end
+
 
   def show
     @item =Item.find(params[:id])
@@ -21,9 +27,21 @@ class ItemsController < ApplicationController
     end
   end 
 
+  def update
+    @item =Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :item_explanation, :condition_id, :category_id, :send_money_id, :todoufukenn_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
-
+  def contributor_confirmation
+    @item =Item.find(params[:id])
+    redirect_to root_path unless @item.user == current_user
+  end
 end
